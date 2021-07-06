17ce17aafa3dbde2a5ccd5ab7a5a61c0b1300b36
Brandon Baars <brandonbaars@gmail.com>
2021-07-06 11:39:33 -0400

    Initial Commit

---
+++ AnimatedChat/AnimatedChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedChatApp.swift
+//  AnimatedChat
+//
+//  Created by Brandon Baars on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
